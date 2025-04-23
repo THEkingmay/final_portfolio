a0ef54a4d48aaea0018c6b517bfff715839ee27d
THEkingmay <beerranee@gmail.com>
2025-04-23 15:26:17 +0700

    add projects

--- src/Data/Project.js
+++ src/Data/Project.js
@@ -1,32 +1,34 @@
 const ProjectData = [
     {
-        title: "Text1",
-        description:"sdhiosuhfdfdfjr59374937493749ysoidyhisodiosdhosidhois\nfdofjdofodfjodfjdo",
-        link:"#",
-        code:"#",
+        title: "ur-cash-is-flow",
+        description: "React + Tailwind\nManage your personal finances with ease.\nYour data is safely stored in localStorage.",
+        link:"https://ur-cash-is-flow.vercel.app/",
+        code:"https://github.com/THEkingmay/UrCashIsFlow",
     },
     {
-        title: "Text2",
-        description:"sdhiosufvdvjhidvhikvhiduhviudvhiu\ndvmd;lvpdvmdhysoefefe583445935793idyhisodiosdhosidhois\nfdofjdofodfjodfjd",
-        link:"#",
-        code:"#",
+        title: "i_will_do_it_later",
+        description: "React + Tailwind\nwith daisyUI for clean UI.\nA simple to-do list app\n stores your data in localStorage.\nAdd, edit, and delete your tasks easily.",
+        link:"https://illdoitlater.vercel.app/",
+        code:"https://github.com/THEkingmay/todo_react-tailwind_daisyUI",
     },
     {
-        title: "Text3",
-        description:"sdhiosuhysoidyhisodiosdhosidhois\nfdofjdofodfjodfjdo",
-        link:"#",
-        code:"#",
-    },{
-        title: "Text4",
-        description:"sdhiosufdfdfdfdysoidyhisodiosdhosidhois\nfdofjdofodfjodfjdo",
-        link:"#",
-        code:"#",
-    },{
-        title: "Text5",
-        description:"sdhiosuhysrgrtrtgrgroidyhisodiosdhosidhois\nfdofjdofodfjodfjdo",
-        link:"#",
-        code:"#",
+        title: "study-class",
+        description:"Built with Vue and Tailwind.\nEasily manage your class schedule\n add details like time, room, and notes.",
+        link:"https://study-class.netlify.app/",
+        code:"https://github.com/THEkingmay/Univ-Class",
     },
+    {
+        title: "warewolf web game",
+        description: "Play the classic Werewolf board game \nHTML/CSS/JS-based.\nOne phone, one moderator, multiple players.\nFind the werewolves before it's too late!",
+        link:"https://matheewarewolf.netlify.app/",
+        code:"https://github.com/THEkingmay/WarewoftV1",
+    },
+    {
+        title: "dreamweave-collective-journey",
+        description: "Create hilarious stories with friends!\nEach player continues the tale \nfrom a random image.\nSimple game made with HTML, CSS, and JS.",
+        link:"https://dreamweave-collective-journey.netlify.app/",
+        code:"",
+    }
 ]
 
 export default ProjectData;
--- src/Views/Project.jsx
+++ src/Views/Project.jsx
@@ -5,13 +5,15 @@ export default function Project({projects}) {
         <div className="h-screen p-4  text-[var(--text-color)] flex items-center flex-col md:justify-center">
             <div className="text-head font-bold ">PROJECTS</div>
                 <div className="m-4 shadow-lg rounded w-full h-full flex flex-col  p-5 md:h-2/3">
-                    <div className="w-full flex font-bold   md:h-1/6 items-center pl-3 pt-3 text-lg">{projects[currentIndex].title}</div>
-                    <div className="w-full whitespace-pre-line flex h-2/5 overflow-scroll mt-2 md:h-3/6 pl-3 text-lg">
+                    <div className="w-full flex font-bold text-3xl underline  md:h-1/6 items-center pl-3 pt-3 text-lg">{projects[currentIndex].title}</div>
+                    <div className="w-full whitespace-pre-line flex h-2/5  mt-2 md:h-3/6 pl-3 text-lg">
                         {projects[currentIndex].description}
                     </div>
                     <div className="underline  flex w-full  p-4 text-lg  md:h-1/6 text-2xl">
-                        <a className="mr-10 md:mr-5" href={projects[currentIndex].link}>Try{">"} </a>
-                        <a href={projects[currentIndex].code}>code{">"}</a>
+                        <a target="blank" className="mr-10 md:mr-5" href={projects[currentIndex].link}>Try{">"} </a>
+                        {projects[currentIndex].code ? 
+                        (<a target="_blank" href={projects[currentIndex].code}> code {">"}</a>) : 
+                        (<span>No code for this project.</span> )}
                     </div>
                     <div className="w-full flex justify-between  md:h-1/6">
                         <button
